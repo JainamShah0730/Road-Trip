d883f22ce2db31d96456962c55e3b8c0f4b066a5
Jainam Shah <jainamshah1527@gmail.com>
2026-08-12 19:57:03 +0530

    new folder create roadtrip game server

--- roadtrip-client/src/App.jsx
+++ roadtrip-client/src/App.jsx
@@ -1,9 +1,36 @@
 import { Canvas } from '@react-three/fiber';
 import RoadNetwork from './RoadNetwork';
 import Van from './Van';
+import { useEffect } from 'react';
+import { io } from 'socket.io-client';
 
 function App() {
+
+  useEffect(() => {
+    const socket = io('http://localhost:5000')
+
+    socket.on('connect', () => {
+      console.log('Connected to game server:', socket.id)
+      socket.emit('join-room', 'TEST123')
+    })
+
+    socket.on('room-state', (players) => {
+      console.log("Current room state:", players)
+    })
+
+    socket.on('player-joined', (player) => {
+      console.log('Player joined:', player)
+    })
+
+    socket.on('player-left', ({ playerId }) => {
+      console.log('Player left:', playerId)
+    })
+    return () => socket.disconnect();
+
+  }, [])
+
   return (
+
     <div style={{ width: '100vw', height: '100vh' }}>
       <Canvas camera={{ position: [0, 8, 12], fov: 60 }}>
         <ambientLight intensity={0.6} />
@@ -19,6 +46,7 @@ function App() {
       </Canvas>
     </div>
   );
+
 }
 
 export default App;
---
+++ roadtrip-game-server/index.js
@@ -0,0 +1,60 @@
+import express from 'express'
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+
+dotenv.config()
+
+const app = express()
+app.use(cors())
+
+const rooms = {}
+const httpServer = createServer(app)
+const io = new Server(httpServer, {
+    cors: { origin: '*' }
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/health', (req, res) => {
+    res.json({ status: 'ok' })
+})
+
+io.on('connection', (socket) => {
+    console.log(`Player connected: ${socket.id}`)
+
+    socket.on('join-room', (roomCode) => {
+        socket.join(roomCode)
+        socket.data.roomCode = roomCode
+
+        if (!rooms[roomCode]) {
+            rooms[roomCode] = { players: {} }
+        }
+        rooms[roomCode].players[socket.id] = { x: 0, y: 0.5, z: 0, rotationY: 0 }
+
+        console.log(`${socket.id} joined room ${roomCode}. Players in room: ${Object.keys(rooms[roomCode].players).length}`)
+
+        socket.emit('room-state', rooms[roomCode].players)
+
+        socket.to(roomCode).emit('player-joined', { playerId: socket.id, ...rooms[roomCode].players[socket.id] })
+    })
+
+    socket.on('disconnect', () => {
+        console.log(`Player disconnected: ${socket.id}`)
+        const roomCode = socket.data.roomCode
+        if (roomCode && rooms[roomCode]) {
+            delete rooms[roomCode].players[socket.id]
+            socket.to(roomCode).emit('player-left', { playerId: socket.id })
+
+            if (Object.keys(rooms[roomCode].players).length === 0) {
+                delete rooms[roomCode]
+            }
+        }
+    })
+})
+
+httpServer.listen(PORT, () => {
+    console.log(`game-server running on port ${PORT}`)
+})
